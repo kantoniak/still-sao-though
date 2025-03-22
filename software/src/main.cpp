291cec7863de3fcd824a175e206c51072276705e
Krzysztof Antoniak <kantoniak@kantoniak.com>
2025-03-22 13:44:19 -0700

    Blink LEDs interchangeably

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -3,19 +3,19 @@
 constexpr uint8_t PIN_LED_1 = 1;
 constexpr uint8_t PIN_LED_2 = 4;
 
+bool pin_led_1_state = LOW;
+
 void setup() {
-  // Init and turn off LEDs
+  // Init and start LEDs
   pinMode(PIN_LED_1, OUTPUT);
   pinMode(PIN_LED_2, OUTPUT);
-  digitalWrite(PIN_LED_1, LOW);
-  digitalWrite(PIN_LED_2, LOW);
+  digitalWrite(PIN_LED_1, pin_led_1_state);
+  digitalWrite(PIN_LED_2, !pin_led_1_state);
 }
 
 void loop() {
-  digitalWrite(PIN_LED_1, HIGH);
-  digitalWrite(PIN_LED_2, HIGH);
-  delay(500);
-  digitalWrite(PIN_LED_1, LOW);
-  digitalWrite(PIN_LED_2, LOW);
-  delay(500);
+  delay(750);
+  pin_led_1_state = !pin_led_1_state;
+  digitalWrite(PIN_LED_1, pin_led_1_state);
+  digitalWrite(PIN_LED_2, !pin_led_1_state);
 }
